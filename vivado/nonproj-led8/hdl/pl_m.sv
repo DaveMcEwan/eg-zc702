99921dba3f3004f23fa509d60fdafee01deb20ab
Dave McEwan <dm16886@it066387.davv.cs.bris.ac.uk>
2017-11-24 18:50:48 +0000

    nonproj-led8 Use AXI GP0 to read/write LED values, like projmode-led8.

--- vivado/nonproj-led8/hdl/pl_m.sv
+++ vivado/nonproj-led8/hdl/pl_m.sv
@@ -1,6 +1,11 @@
 
 `include "ff.svh"
 
+`define AXI_RESP_OKAY   2'b00
+`define AXI_RESP_EXOKAY 2'b01
+`define AXI_RESP_SLVERR 2'b10
+`define AXI_RESP_DECERR 2'b11
+
 module pl_m (
   // clkrst
   input  wire       i_clk0,
@@ -144,10 +149,52 @@ module pl_m (
   output wire [7:0] o_led
 );
 
+  // Always accept write address, write data, and read request.
+  // Address is ignored, just store/return last written data word.
+  assign o_M_AXI_GP0_AWREADY = 1'b1;
+  assign o_M_AXI_GP0_WREADY = 1'b1;
+  assign o_M_AXI_GP0_ARREADY = 1'b1;
+
+  // Store/return last written data word, display on LEDs.
+  `ff_cg_arst (logic [31:0], gp0_wdata, i_clk0, i_M_AXI_GP0_WVALID, i_rst, '0)
+  always_comb gp0_wdata_d = i_M_AXI_GP0_WDATA;
+  assign o_led = gp0_wdata_q[7:0];
+  assign o_M_AXI_GP0_RDATA = gp0_wdata_q;
+
+  // Response to write is always OKAY.
+  assign o_M_AXI_GP0_BRESP = `AXI_RESP_OKAY;
+  `ff_nocg_arst (logic, gp0_bvalid, i_clk0, i_rst, '0)
+  always_comb
+    if      (i_M_AXI_GP0_WVALID)  gp0_bvalid_d = 1'b1;
+    else if (i_M_AXI_GP0_BREADY)  gp0_bvalid_d = 1'b0;
+    else                          gp0_bvalid_d = gp0_bvalid_q;
+  assign o_M_AXI_GP0_BVALID = gp0_bvalid_q;
+
+  // Response to read is always OKAY.
+  assign o_M_AXI_GP0_RRESP = `AXI_RESP_OKAY;
+  `ff_nocg_arst (logic, gp0_rvalid, i_clk0, i_rst, '0)
+  always_comb
+    if      (i_M_AXI_GP0_ARVALID) gp0_rvalid_d = 1'b1;
+    else if (i_M_AXI_GP0_RREADY)  gp0_rvalid_d = 1'b0;
+    else                          gp0_rvalid_d = gp0_rvalid_q;
+  assign o_M_AXI_GP0_RVALID = gp0_rvalid_q;
+  assign o_M_AXI_GP0_RLAST = gp0_rvalid_q && i_M_AXI_GP0_RREADY;
+
+  // Reflect IDs
+  `ff_cg_arst (logic [11:0], gp0_awid, i_clk0, i_M_AXI_GP0_AWVALID, i_rst, '0)
+  `ff_cg_arst (logic [11:0], gp0_arid, i_clk0, i_M_AXI_GP0_ARVALID, i_rst, '0)
+  always_comb gp0_awid_d = i_M_AXI_GP0_AWID;
+  always_comb gp0_arid_d = i_M_AXI_GP0_ARID;
+  assign o_M_AXI_GP0_BID = gp0_awid_q;
+  assign o_M_AXI_GP0_RID = gp0_arid_q;
+
+  /*
   `ff_nocg_arst (logic [31:0], cntr, i_clk0, i_rst, '0)
   always_comb cntr_d = cntr_q + 1;
 
   assign o_led = cntr_q[31:24];
+  */
+
 
 endmodule
 
